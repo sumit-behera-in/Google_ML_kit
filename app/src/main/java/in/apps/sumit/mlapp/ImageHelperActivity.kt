52265ee5ddf9f53f9f0698b85eb468fdd8015da4
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-02-12 14:08:46 +0530

    init

---
+++ app/src/main/java/in/apps/sumit/mlapp/ImageHelperActivity.kt
@@ -0,0 +1,11 @@
+package `in`.apps.sumit.mlapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ImageHelperActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_image_helper)
+    }
+}
---
+++ app/src/main/java/in/apps/sumit/mlapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package `in`.apps.sumit.mlapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import `in`.apps.sumit.mlapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageHelper.setOnClickListener {
+            val intent = Intent(this,ImageHelperActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
